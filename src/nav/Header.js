5d053ea9d4c54e80f7e929bf5a8f5524c7d27533
Joseph Mutua <mutuaj793@gmail.com>
2021-01-30 10:59:34 +0300

    Add react router link for menu items

--- src/nav/Header.js
+++ src/nav/Header.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
 import { SettingOutlined, AppstoreOutlined, UserOutlined, UserAddOutlined } from "@ant-design/icons";
+import {Link} from "react-router-dom";
+
 
 const { SubMenu , Item } = Menu;
 
@@ -14,19 +16,14 @@ const Header = () => {
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
       <Item key="home" icon={<AppstoreOutlined />}>
-        Home
+        <Link to="/">Home</Link>
       </Item>
 
+      <Item key="register" icon={<UserAddOutlined />} className="float-right">
+        <Link to="/register">Register</Link>
+      </Item>
       <Item key="login" icon={<UserOutlined />} className="float-right">
-        Login
-      </Item>
-
-      <Item
-        key="register"
-        icon={<UserAddOutlined />}
-        className="float-right"
-      >
-        Register
+        <Link to="/login">Login</Link>
       </Item>
 
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
