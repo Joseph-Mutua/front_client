852e1ba8aab9bda2b230ce472e5593dbc7094ba0
Joseph Mutua <mutuaj793@gmail.com>
2021-03-16 19:19:03 +0300

    Category create and list

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -1,14 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import {
+  createCategory,
+  getCategories,
+  removeCategory,
+} from "../../../helpers/category";
 
 const CategoryCreate = () => {
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((cat) => setCategories(cat.data));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+
+    setLoading(true);
+
+    createCategory({ name }, user.token)
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+        setName("");
+        toast.success(`${res.data.name} is created`);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        if (err.response.status === 400) toast.error(err.response.data);
+      });
+  };
+
+  const categoryForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          type="text"
+          className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          autoFocus
+          required
+        />
+        <br />
+        <button className="btn btn-outline-primary">Save</button>
+      </div>
+    </form>
+  );
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">Category Create Page</div>
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4 className="text-center">Create Category</h4>
+          )}
+
+          {categoryForm()}
+
+          <hr />
+
+          {categories.map((c) => (
+            <div key={c._id}>{c.name}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
