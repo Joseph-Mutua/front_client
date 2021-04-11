024e37e11fe8c7094a336acfd0fede033ca17255
Joseph Mutua <mutuaj793@gmail.com>
2021-04-11 17:58:36 +0300

    Get product on update page

--- src/functions/product.js
+++ src/functions/product.js
@@ -17,3 +17,6 @@ export const deleteProduct = async (slug, authtoken) =>
       authtoken,
     },
   });
+
+  export const getProduct = async (slug) =>
+    await axios.get(`${process.env.REACT_APP_API}/product/${slug}`);
--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -2,15 +2,46 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createProduct } from "../../../functions/product";
+import { getProduct } from "../../../functions/product";
 import { getCategories, getSubCategories } from "../../../functions/category";
 import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 
 import { LoadingOutlined } from "@ant-design/icons";
 
+const initialState = {
+  title: "",
+  description: "",
+  price: "",
+  categories: [],
+  category: "",
+  subcategories: [],
+  shipping: "",
+  quantity: "",
+  images: [],
+  colors: ["Black", "Brown", "Silver", "White", "Blue"],
+  brands: ["Apple", "Samsung", "Microsoft"],
+  color: "",
+  brand: "",
+};
+
 const ProductUpdate = ({ match }) => {
+  //State
+  const [values, setValues] = useState(initialState);
+
   //Redux
   const { user } = useSelector((state) => ({ ...state }));
+  const { slug } = match.params;
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getProduct(slug).then((p) => {
+      console.log("Single Product", p);
+      setValues({ ...values, ...p.data });
+    });
+  };
 
   return (
     <div className="container-fluid">
@@ -22,8 +53,9 @@ const ProductUpdate = ({ match }) => {
         <div className="col-md-10">
           <h4 className="text-center">Update Product</h4>
 
-          {JSON.stringify(match.params.slug)}
-          
+          {JSON.stringify(slug)}
+          {JSON.stringify(values)}
+
           <hr />
         </div>
       </div>
