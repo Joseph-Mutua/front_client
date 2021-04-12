35b48248c0444c019ca5aa0cf8c85a291138ff5e
Joseph Mutua <mutuaj793@gmail.com>
2021-04-12 15:13:30 +0300

    Add typewriter effect in jumbotron

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getProductsByCount } from "../functions/product";
 import ProductCard from "../components/cards/ProductCard";
+import Jumbotron from "../components/cards/Jumbotron";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -21,21 +22,17 @@ const Home = () => {
 
   return (
     <>
-      <div className="jumbotron">
-        {loading ? (
-          <h4 className="text-center text-primary">Loading...</h4>
-        ) : (
-          <h4 className="text-center">All Products</h4>
-        )}
+      <div className="jumbotron text-danger h1 font-weight-bold text-center">
+        <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
       </div>
 
       <div className="container">
         <div className="row">
-          {products.map((product) => 
+          {products.map((product) => (
             <div key={product._id} className="col-md-4">
               <ProductCard product={product} />
             </div>
-          )}
+          ))}
         </div>
       </div>
     </>
