076ecfe9ae7268bc19444910f1d8f85b5c87c18a
Joseph Mutua <mutuaj793@gmail.com>
2021-04-13 18:12:58 +0300

    Single product component

---
+++ src/components/cards/SingleProduct.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Card } from "antd";
+import { Link } from "react-router-dom";
+import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+
+const { Meta } = Card;
+
+const SingleProduct = ({ product }) => {
+  const { title, description, images, slug } = product;
+
+  return (
+    <>
+      <div className="col-md-7">Image Carousel</div>
+      <div className="col-md-5">
+        <Card
+          actions={[
+            <>
+              <ShoppingCartOutlined className="text-success" /> <br />
+              Add to Cart
+            </>,
+            <Link to="/">
+              <HeartOutlined className="text-info" /> <br />
+              Add to Wishlist
+            </Link>,
+          ]}
+        >
+          <Meta title={title} description={description} />
+          <p>
+            Price /Category /Subcategories /Shipping /Color /Brand /Quantity
+            Available /{" "}
+          </p>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default SingleProduct;
