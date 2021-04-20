4109cb085e2420a0138b69df87832a2d28363181
Joseph Mutua <mutuaj793@gmail.com>
2021-04-20 16:42:41 +0300

    Cart page setup

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import Product from "./pages/Product";
 import CategoryHome from "./pages/category/CategoryHome";
 import SubCategoryHome from "./pages/subcategory/SubCategoryHome";
 import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -116,6 +117,7 @@ const App = () => {
         <Route exact path="/category/:slug" component={CategoryHome} />
         <Route exact path="/subcategory/:slug" component={SubCategoryHome} />
         <Route exact path="/shop" component={Shop} />
+        <Route exact path="/cart" component={Cart} />
       </Switch>
     </>
   );
