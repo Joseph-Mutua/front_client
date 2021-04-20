5595230a8119c152035ed5f0e7d4977f74b20c52
Joseph Mutua <mutuaj793@gmail.com>
2021-04-20 17:12:13 +0300

    Cart page order summary

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,13 +4,51 @@ import { Link } from "react-router-dom";
 
 const Cart = () => {
   const { cart, user } = useSelector((state) => ({ ...state }));
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
+  const getTotal = () => {
+    return cart.reduce((currentVal, nextVal) => {
+      return currentVal + nextVal.count * nextVal.price;
+    }, 0);
+  };
 
   return (
-    <div classNammme="container-fluid">
+    <div classNammme="container-fluid pt-4">
       <div className="row">
-        <h4>Cart</h4>
+        <div className="col-md-8">
+          <h4>Cart / {cart.length} Products</h4>
+          {!cart.length ? (
+            <p>
+              No Products in Cart. <Link to="/shop">Continue Shopping</Link>
+            </p>
+          ) : (
+            "Show Cart items"
+          )}
+        </div>
+        <div className="col-md-4">
+          <h4>Order Surmary</h4>
+          <hr />
+          <p>Products</p>
+          {cart.map((c, i) => (
+            <div key={i}>
+              <p>
+                {c.title} x {c.count} = ${c.price * c.count}
+              </p>
+            </div>
+          ))}
+          <hr />
+          Total: <b>${getTotal()}</b>
+          <hr />
+          {user ? (
+            <button className="btn btn-sm btn-primary mt-2">
+              Proceed to Checkout
+            </button>
+          ) : (
+            <button className="btn btn-sm btn-primary mt-2">
+              Login to Checkout
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
