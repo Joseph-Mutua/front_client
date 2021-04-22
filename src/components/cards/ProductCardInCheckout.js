140c64da688756f0608b75f85c423269a64de1ac
Joseph Mutua <mutuaj793@gmail.com>
2021-04-22 14:53:56 +0300

    Remove products from cart

--- src/components/cards/ProductCardInCheckout.js
+++ src/components/cards/ProductCardInCheckout.js
@@ -2,6 +2,12 @@ import React from "react";
 import ModalImage from "react-modal-image";
 import Monitor from "../../images/monitor.png";
 import { useDispatch } from "react-redux";
+import {
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
+import { toast } from "react-toastify";
 
 const ProductCardInCheckout = ({ p }) => {
   const colors = ["Black", "Brown", "Silver", "White", "Blue"];
@@ -33,6 +39,55 @@ const ProductCardInCheckout = ({ p }) => {
     }
   };
 
+  const handleQuantityChange = (e) => {
+    let count = e.target.value < 1 ? 1 : e.target.value;
+
+    if (count > p.quantity) {
+      toast.error(`Max available quantity: ${p.quantity}`);
+      return;
+    }
+
+    let cart = [];
+
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      cart.map((product, i) => {
+        if (product._id == p._id) {
+          cart[i].count = count;
+        }
+      });
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
+  const handleRemove = () => {
+    let cart = [];
+
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      cart.map((product, i) => {
+        if (product._id === p._id) {
+          cart.splice(i, 1);
+        }
+      });
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
   return (
     <tbody>
       <tr>
@@ -53,7 +108,7 @@ const ProductCardInCheckout = ({ p }) => {
             onChange={handleColorChange}
             className="form-control"
             name="color"
-            id={p._id}
+            id=""
           >
             {p.color ? (
               <option value={p.color}>{p.color}</option>
@@ -63,15 +118,33 @@ const ProductCardInCheckout = ({ p }) => {
             {colors
               .filter((c) => c !== p.color)
               .map((c) => (
-                <option value={c} key={c}>
+                <option value={c} key={p}>
                   {c}
                 </option>
               ))}
           </select>
         </td>
-        <td>{p.count} </td>
-        <td>Shipping</td>
-        <td>Delete Icon</td>
+        <td className="text-center">
+          <input
+            type="number"
+            className="form-control"
+            value={p.count}
+            onChange={handleQuantityChange}
+          />
+        </td>
+        <td className="text-center">
+          {p.shipping === "Yes" ? (
+            <CheckCircleOutlined className="text-success" />
+          ) : (
+            <CloseCircleOutlined className="text-danger" />
+          )}
+        </td>
+        <td className="text-center">
+          <CloseOutlined
+            onClick={handleRemove}
+            className="text-danger pointer"
+          />
+        </td>
       </tr>
     </tbody>
   );
