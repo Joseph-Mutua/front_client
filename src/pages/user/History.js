2b93bb7837a4a692b0f6c4a55d8e9b49f2524a76
Joseph Mutua <mutuaj793@gmail.com>
2021-04-29 13:56:33 +0300

    Showing each orders products in table

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -4,6 +4,7 @@ import { getUserOrders } from "../../functions/user";
 import { useSelector, useDispatch } from "react-redux";
 import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import { toast } from "react-toastify";
+import ProductCardInCheckout from "../../components/cards/ProductCardInCheckout";
 
 const History = () => {
   const [orders, setOrders] = useState([]);
@@ -20,7 +21,41 @@ const History = () => {
       setOrders(res.data);
     });
 
-  const showOrderInTable = (order) => <p>Each Order and its products</p>;
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td>{p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === "Yes" ? (
+                <CheckCircleOutlined style={{ color: "green" }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: "red" }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 
   const showEachOrder = () =>
     orders.map((order, i) => (
